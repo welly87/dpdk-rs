7f485afc4ee29997267ed301c6f2e451917d96b9
Pedro Henrique Penna <pedrohenriquepenna@outlook.com>
2022-05-30 16:03:06 -0300

    [build] Workaround: Adding Allow Annotations

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+#![cfg_attr(feature = "strict", deny(clippy:all))]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused)]
+#![allow(unaligned_references)]
+
 use std::os::raw::{
     c_char,
     c_int,
