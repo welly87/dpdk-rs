dd2f79594d6bb775380e65ceeaf52c2c393cc7ad
Pedro Henrique Penna <ppenna@microsoft.com>
2022-07-26 11:10:57 -0300

    [build] Feature: Build System for Windows

---
+++ linux.mk
@@ -0,0 +1,34 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+#=======================================================================================================================
+# Default Paths
+#=======================================================================================================================
+
+export PREFIX ?= $(HOME)
+export PKG_CONFIG_PATH ?= $(shell find $(PREFIX)/lib/ -name '*pkgconfig*' -type d | xargs | sed -e 's/\s/:/g')
+
+#=======================================================================================================================
+# Tools
+#=======================================================================================================================
+
+export CARGO ?= $(HOME)/.cargo/bin/cargo
+export RM ?= rm -rf
+
+#=======================================================================================================================
+# Switches
+#=======================================================================================================================
+
+# Set build mode.
+ifneq ($(DEBUG),yes)
+export BUILD = release
+else
+export BUILD = dev
+endif
+
+# Set build flags.
+export FLAGS += --profile $(BUILD)
+
+# Set driver version.
+export DRIVER ?= $(shell [ ! -z "`lspci | grep -E "ConnectX-[4,5]"`" ] && echo mlx5 || echo mlx4)
+export FLAGS += --features=$(DRIVER)
